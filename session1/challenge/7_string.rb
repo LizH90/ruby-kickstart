6b00e3c4f4692c0ef96d467e3e29a169398f574b
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-07-12 19:59:28 +0100

    workingon 1:6 & 1:7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,13 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  string.times.do |i| i=r
+  string << i
 end
+
+
+pirates_say_arrrrrrrrr("are you really learning Ruby?")
